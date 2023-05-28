d0370db8fb37eaa08fdbb6b07007210282307572
Emeika <shahbazhafsa2014@gmail.com>
2023-05-28 18:56:18 +0500

    FloatComplex.h

---
+++ FloatComplex.h
@@ -0,0 +1,33 @@
+#ifndef FloatComplex_H
+#define FloatComplex_H
+#include "IntComplex.h"
+
+class FloatComplex : public IntComplex
+{
+private:
+    float real, imag;
+
+public:
+    // Constructors
+    FloatComplex();
+
+    FloatComplex(const float &, const float &);
+
+    FloatComplex(const FloatComplex &);
+
+    ~FloatComplex();
+
+    // Operator overload functions
+    FloatComplex &operator+(const FloatComplex &);
+
+    FloatComplex &operator-(const FloatComplex &);
+
+    FloatComplex &operator*(const FloatComplex &);
+
+    FloatComplex &operator/(const FloatComplex &);
+
+    // Inherit the print function from the base class
+    using IntComplex::print;
+};
+
+#endif
